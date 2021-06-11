e30e54b01a21239a3d3f262b0c86ea79c8bc2824
Andrew Bowers <andrew.bowers@agamecoder.com>
2021-06-11 15:10:22 -0700

    Better organization and ability to set parameters.

--- Brother.gd
+++ Brother.gd
@@ -3,7 +3,7 @@ extends KinematicBody2D
 signal broadcast_pos(pos)
 
 var other_brother_pos = Vector2(0, 0)
-export(int) var connection_force = 100
+export(int) var connection_force = 1
 export(int) var speed = 200
 export(String) var up_btn = "p1_up"
 export(String) var down_btn = "p1_down"
@@ -15,6 +15,7 @@ func listen_other_pos(pos):
 
 
 func _process(delta):
+	var magnetic_mult = global_position.distance_to(other_brother_pos) * connection_force
 	var magnetic_force = global_position.direction_to(other_brother_pos).normalized()
 	var control_force = Vector2(0, 0)
 	if Input.is_action_pressed(up_btn):
@@ -27,6 +28,6 @@ func _process(delta):
 		control_force.x += 1
 	control_force = control_force.normalized()
 	# include controls force too
-	var impulse = (magnetic_force * connection_force) + (control_force * speed)
+	var impulse = (magnetic_force * magnetic_mult) + (control_force * speed)
 	move_and_slide(impulse)
 	emit_signal("broadcast_pos", global_position)
