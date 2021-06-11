3f1b6ab841b244dbd071a1ba4f6421fa9677b63d
Andrew Bowers <andrew.bowers@agamecoder.com>
2021-06-11 13:52:26 -0700

    Initial controls

--- Brother.gd
+++ Brother.gd
@@ -4,6 +4,11 @@ signal broadcast_pos(pos)
 
 var other_brother_pos = Vector2(0, 0)
 export(int) var connection_force = 100
+export(int) var speed = 200
+export(String) var up_btn = "p1_up"
+export(String) var down_btn = "p1_down"
+export(String) var left_btn = "p1_left"
+export(String) var right_btn = "p1_right"
 
 func listen_other_pos(pos):
 	other_brother_pos = pos
@@ -11,6 +16,17 @@ func listen_other_pos(pos):
 
 func _process(delta):
 	var magnetic_force = global_position.direction_to(other_brother_pos).normalized()
+	var control_force = Vector2(0, 0)
+	if Input.is_action_pressed(up_btn):
+		control_force.y -= 1
+	if Input.is_action_pressed(down_btn):
+		control_force.y += 1
+	if Input.is_action_pressed(left_btn):
+		control_force.x -= 1
+	if Input.is_action_pressed(right_btn):
+		control_force.x += 1
+	control_force = control_force.normalized()
 	# include controls force too
-	move_and_slide(magnetic_force * connection_force)
+	var impulse = (magnetic_force * connection_force) + (control_force * speed)
+	move_and_slide(impulse)
 	emit_signal("broadcast_pos", global_position)
